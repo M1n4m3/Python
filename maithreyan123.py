7fb5697c178efe4ce2c66a35b21e868a45420eb8
Maithreyan123 <smaithreyan1@gmail.com>
2025-07-18 10:43:58 +0530

    Maithreyan
    we successfully uploaded the file.

---
+++ maithreyan123.py
@@ -0,0 +1,14 @@
+class mom():
+    name="dency peter"
+    age=20
+    city="london"
+    phone_no=9488455605
+    def city(self):
+        print("4-6 city")
+class son(mom):
+    def laptop(self):
+        print("son's laptop")  
+son1=son()
+son1.city()
+print(son1.age)
+print(son1.name)              
